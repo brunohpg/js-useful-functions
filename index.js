e1a591aab6efdeedd405e53db0b6d5e957696ad7
Eduardo <eduardohds.dev@outlook.com>
2020-07-17 20:40:35 -0300

    Added formatDateBR

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Format Date object to Brazil format
+ *
+ * @param {Date} date
+ * @returns {string}
+ */
+function formatDateBR(date) {
+    let month = date.getMonth() + 1;
+    month = month.toString().length < 2 ? `0${month}` : month;
+    return `${date.getDate()}/${month}/${date.getFullYear()}`;
+}
+
+module.exports = {
+    formatDateBR
+};
