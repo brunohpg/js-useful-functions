eaefb7583d7ef8842680d52283b037713903a546
Eduardo Henrique <eduardohds.dev@outlook.com>
2020-08-09 13:36:05 -0300

    Fix day with two caracteres

--- index.js
+++ index.js
@@ -4,9 +4,11 @@
  * @returns {string} date as DD/MM/YYYY
  */
 function formatDateBR(date) {
+    let day = date.getDate();
+    day = day.toString().length < 2 ? `0${day}` : day;
     let month = date.getMonth() + 1;
     month = month.toString().length < 2 ? `0${month}` : month;
-    return `${date.getDate()}/${month}/${date.getFullYear()}`;
+    return `${day}/${month}/${date.getFullYear()}`;
 }
 
 /**
