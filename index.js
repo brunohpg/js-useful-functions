2fc8e039ffc719814fa587c248ad35b28f9d4798
eduhds <eduardohds.dev@outlook.com>
2020-11-26 19:36:30 +0000

    Add data mask text

--- index.js
+++ index.js
@@ -70,6 +70,15 @@ function maskText(maskType, onChangeText) {
                         .replace(/(-\d{3})\d+?$/, '$1')
                 );
                 break;
+            case 'data':
+                onChangeText(
+                    text
+                        .replace(/\D/g, "")
+                        .replace(/(\d{2})(\d)/, "$1/$2")
+                        .replace(/(\d{2})(\d)/, "$1/$2")
+                        .replace(/(\d{4})\d+?$/, "$1")
+                );
+                break;
             default:
                 break;
         }
