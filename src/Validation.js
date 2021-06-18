10168ee08eab8ba4e54692e5b191162b9d251be5
eduhds <eduardohds.dev@outlook.com>
2021-06-18 14:09:03 +0000

    Validation functions

---
+++ src/Validation.js
@@ -0,0 +1,22 @@
+// https://pt.stackoverflow.com/questions/11045/express%C3%A3o-regular-para-validar-um-campo-que-aceita-cpf-ou-cnpj
+
+function emailIsValid(email) {
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return reg.test(email);
+}
+
+function cpfIsValid(cpf) {
+    let reg = /[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/
+    return reg.test(cpf)
+}
+
+function cnpjIsValid(cnpj) {
+    let reg = /[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2}/
+    return reg.test(cnpj)
+}
+
+function cpfcnpjIsValid(cpfcnpj) {
+    let reg =
+        /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/;
+    return reg.test(cpfcnpj);
+}
