7c317b836704594b270accaa041b60bbe875df22
Eduardo Henrique <eduardohds.dev@outlook.com>
2021-08-30 15:47:37 -0300

    Correção return sortByKey

--- index.js
+++ index.js
@@ -113,6 +113,7 @@ function sortByKey(k) {
         var textB = b[k];
         if (typeof textA === 'string') textA = textA.toUpperCase().trim();
         if (typeof textB === 'string') textB = textB.toUpperCase().trim();
+	return textA < textB ? -1 : textA > textB ? 1 : 0;
     };
 }
 
